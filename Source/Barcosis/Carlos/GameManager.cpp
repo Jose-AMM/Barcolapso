b6ee8b60d0d5ab0f265bd6a88ba2e91a75c5823d
cmartinezgom <carlosmargom1996@gmail.com>
2025-04-05 19:10:27 +0200

    Fixed C++ folders, created mine, created GameManager and added Third Person Template
    Fixed C++ folders, created mine, created GameManager and added Third Person Template

---
+++ Source/Barcosis/Carlos/GameManager.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Carlos/GameManager.h"
+
+void UGameManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+}
+
+void UGameManager::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UGameManager::LoadBattleLevelInstance()
+{
+
+}
---
+++ Source/Barcosis/Carlos/GameManager.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GameManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BARCOSIS_API UGameManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	void LoadBattleLevelInstance();
+
+};
